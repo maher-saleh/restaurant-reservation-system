d9c3c8a190eea1fbd310c179d4e3b72035493851
Maher Saleh <ehabelnahas@hotmail.com>
2025-10-01 10:08:55 +0300

    Initial commit with Vue + serverless API

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,12 @@
+// /api/proxy.js
 export default async function handler(req, res) {
     const token = process.env.API_TOKEN;
     const baseUrl = "https://api.foodics.dev/v5";
-    const path = req.query.path ? req.query.path.join("/") : "";
-    const query = new URLSearchParams(req.query);
-    query.delete("path");
-    const url = `${baseUrl}/${path}${query.toString() ? `?${query.toString()}` : ""}`;
+    const fullPath = req.url.split("/api/")[1] || "";
+    const targetUrl = `${baseUrl}/${fullPath}`;
 
     try {
-        const response = await fetch(url, {
+        const response = await fetch(targetUrl, {
             method: req.method,
             headers: {
                 "Content-Type": "application/json",
