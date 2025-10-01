2b34629ea82fa16a63dd3d036f91850450c1cd33
Maher Saleh <ehabelnahas@hotmail.com>
2025-10-01 09:30:02 +0300

    Initial commit with Vue + serverless API

--- api/[...path].js
+++ api/[...path].js
@@ -1,10 +1,14 @@
 export default async function handler(req, res) {
     const token = process.env.API_TOKEN;
     const baseUrl = "https://api.foodics.dev/v5";
-    const path = req.query.path.join("/");
+    const path = req.query.path ? req.query.path.join("/") : "";
+    const { path: _, ...queryParams } = req.query;
+    const queryString = new URLSearchParams(queryParams).toString();
+
+    const url = `${baseUrl}/${path}${queryString ? `?${queryString}` : ""}`;
 
     try {
-        const response = await fetch(`${baseUrl}/${path}`, {
+        const response = await fetch(url, {
             method: req.method,
             headers: {
                 "Content-Type": "application/json",
